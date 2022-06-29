9cb5a88b372aec81536d13ba38d40f3c1ffee42a
saintpvul <saintpvul@gmail.com>
2022-06-30 01:51:14 +0600

    complete day1 exercises

---
+++ day#1/script/datatypes.js
@@ -0,0 +1,22 @@
+console.log(typeof "it's string");
+console.log(typeof 26);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+let my1stName = "Paul",
+  my2stName = "Shvets",
+  myMaritStatus = "married",
+  myCount = "Kaz",
+  myAg = "27";
+
+let myFirstName = "Paul";
+let myLastName = "Shvets";
+let myMaritalStatus = "married";
+let myCountry = "Kaz";
+let myAge = "27";
+
+let myOwnAge = 27;
+let yourOwnAge = 30;
+console.log("I am " + myOwnAge + " years old.");
+console.log("You are " + yourOwnAge + " years old");
